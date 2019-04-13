3f66ee859d2c03fde1b8571c453868b8ecbf5ae5
Firmino Changani <flowck96@gmail.com>
2019-04-13 23:21:29 +0100

    Add query filters

--- src/controllers/stockHistoriesController.js
+++ src/controllers/stockHistoriesController.js
@@ -2,9 +2,29 @@
 const StockHistories = require("../models/stockHistories");
 
 module.exports.getStocks = async (req, res) => {
+
+  // Query options
+  const findOptions = {};
+  const sort = {};  
+
+  // Query paramaters
+  let { limit, year } = req.query;
+
+  // Check if the year query params was defined
+  if (year) {
+    year = parseInt(year) + 1;
+    year = year.toString();
+    findOptions.date = { $lte: new Date(year).toISOString() };
+  }
+
   try {
-    const stocks = await StockHistories.find({}).limit(100);
-    res.json(stocks);
+    // Query results
+    let stocks = await StockHistories
+      .find(findOptions)
+      .limit(parseInt(limit) || 50)
+
+    // Send the request response 
+    res.status(200).json(stocks);
   } catch (err) {
     // Send an error 400 response
     res.status(400).json(err);
