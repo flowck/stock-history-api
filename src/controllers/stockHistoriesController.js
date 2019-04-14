7cc842914187cfcb67a6d9b88b067075c41ba43c
Firmino Changani <flowck96@gmail.com>
2019-04-14 19:02:16 +0100

    Add caching support

--- src/controllers/stockHistoriesController.js
+++ src/controllers/stockHistoriesController.js
@@ -1,11 +1,11 @@
 // Dependencies
 const StockHistories = require("../models/stockHistories");
+const redisClient = require("../database/cacheDatabseConnection").client;
 
 module.exports.getStocks = async (req, res) => {
-
   // Query options
   const findOptions = {};
-  const sort = {};  
+  const sort = {};
 
   // Query paramaters
   let { limit, year } = req.query;
@@ -18,12 +18,16 @@ module.exports.getStocks = async (req, res) => {
   }
 
   try {
-    // Query results
-    let stocks = await StockHistories
-      .find(findOptions)
-      .limit(parseInt(limit) || 50)
+    // Query stocks in the database
+    let stocks = await StockHistories.find(findOptions).limit(
+      parseInt(limit) || 50
+    );
 
-    // Send the request response 
+    // console.log("EXE");
+
+    // Cache the results in memory for 30 minutes until it expires
+    redisClient.setex("stockHistories", 1800, JSON.stringify(stocks));
+    // Send the request response
     res.status(200).json(stocks);
   } catch (err) {
     // Send an error 400 response
