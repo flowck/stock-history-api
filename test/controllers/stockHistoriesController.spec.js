c44e6a2067d2029a1cff5707fa3ed53f40a8d7ca
Firmino Changani <flowck96@gmail.com>
2019-04-14 14:50:01 +0100

    Fix buggy test:

--- test/controllers/stockHistoriesController.spec.js
+++ test/controllers/stockHistoriesController.spec.js
@@ -9,15 +9,27 @@ process.env.NODE_ENV = "test";
 
 // Setup chai with chai-http
 chai.use(http);
+const should = chai.should();
 
 /**
  * Stocks
  */
 
 describe("Stocks", () => {
-  it ("It should get the initial 100 stocks", (done) => {
+  it ("It should response", (done) => {
     chai.request(app)
       .get("/api/v1/stocks")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("array");
+        res.body.length.should.be.eql(50);
+        done();
+      });
+  });
+
+  it ("It should response with 100 records", (done) => {
+    chai.request(app)
+      .get("/api/v1/stocks?limit=100")
       .end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a("array");
@@ -25,4 +37,5 @@ describe("Stocks", () => {
         done();
       });
   });
+  // process.exit();
 });
